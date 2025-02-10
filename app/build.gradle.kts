e1aa958e62915511d26e278d8b7d731e894f0776
fahad_ld <mdashfahad@gmail.com>
2025-02-10 04:36:26 +0100

    fixed build error due to hilt nav -compose library missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     kapt(libs.room.compiler)
     implementation(libs.room.ktx)
 
-   // implementation(libs.androidx.navigation.navigation.compose)
-    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.accompanist.swiperefresh)
 }
